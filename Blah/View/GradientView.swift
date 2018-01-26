87f9afe642e40b3c42f0816d01e25365453342ad
Sketchy Swift <hafizar788@gmail.com>
2018-01-26 12:32:58 -0500

    Created ChannelVC UI

---
+++ Blah/View/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Blah
+//
+//  Created by Abdurrahman on 1/25/18.
+//  Copyright © 2018 AR Ehsan. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+	@IBInspectable var topColor: UIColor = #colorLiteral(red: 0.431372549, green: 0.4666666667, blue: 0.7960784314, alpha: 1) {
+		didSet {
+			self.setNeedsLayout()
+		}
+	}
+	
+	@IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.2509803922, green: 0.3098039216, blue: 0.431372549, alpha: 1) {
+		didSet {
+			self.setNeedsLayout()
+		}
+	}
+	
+	override func layoutSubviews() {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+		gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+		gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+		gradientLayer.frame = self.bounds
+		self.layer.insertSublayer(gradientLayer, at: 0)
+	}
+}
