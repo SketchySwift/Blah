566d3735b619bb0378fd3dc85f79a037656cd1af
Sketchy Swift <hafizar788@gmail.com>
2018-01-31 19:33:09 -0500

    Created some constants and AuthService class

--- Blah/Utilities/Constants.swift
+++ Blah/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://blahchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = ""
+let USER_EMAIL = "userEmail"
