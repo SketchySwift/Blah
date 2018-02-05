9163d6e7c1ce1722e5b604d189ab9fba98631163
Sketchy Swift <hafizar788@gmail.com>
2018-02-05 17:23:52 -0500

    Cleaned up a couple of things

--- Blah/Controller/CreateAccountViewController.swift
+++ Blah/Controller/CreateAccountViewController.swift
@@ -51,7 +51,7 @@ class CreateAccountViewController: UIViewController {
 	}
 	
 	@IBAction func pickAvatarPressed(_ sender: Any) {
-	
+		performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
 	}
 	
 	@IBAction func pickBgColorPressed(_ sender: Any) {
